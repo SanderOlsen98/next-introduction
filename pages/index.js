d38672d1dcce812d367782b0aee13351c2c60feb
Connor O'Brien <connor@hifranc.com>
2021-03-15 18:38:49 +0400

    Add pre-rendered pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
-import Head from "next/head";
+import Head from "../components/layout/Head";
 import Layout from "../components/layout/Layout";
+import axios from "axios";
+import { BASE_URL } from "../constants/api";
 
-export default function Home() {
+export default function Home(props) {
+	// the log here will happen in the browser console
+	console.log(props);
 	return (
 		<Layout>
 			<Head />
+			<h1>Home page</h1>
 
-			<div className="container">
-				<h1>Home page</h1>
-			</div>
+			{props.games.map((game) => {
+				return <h3 key={game.slug}>{game.name}</h3>;
+			})}
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	// in case there is an error in the API call
+	// we'll send an empty array in as the prop
+	let games = [];
+
+	try {
+		const response = await axios.get(BASE_URL);
+		// the log here will happen on the server, you can check the console in your editor
+		console.log(response.data);
+		// the array is on the response.data.data property
+		games = response.data.data;
+	} catch (error) {
+		console.log(error);
+	}
+
+	// the props object we return here will become the props in the component
+	return {
+		props: {
+			games: games,
+		},
+	};
 }
