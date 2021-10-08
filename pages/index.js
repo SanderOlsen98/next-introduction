982dc773fffa74f331616eb71e4bae1c8a2a168d
Martin Kruger <martin@martinkruger.com>
2021-10-08 14:48:51 +0200

    fix: updated API to Noroff API

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,43 @@
-import Head from "../components/layout/Head";
-import Layout from "../components/layout/Layout";
-import axios from "axios";
-import { BASE_URL } from "../constants/api";
+import Head from '../components/layout/Head';
+import Layout from '../components/layout/Layout';
+import axios from 'axios';
+import { BASE_URL } from '../constants/api';
 
 export default function Home(props) {
-	// the log here will happen in the browser console
-	console.log(props);
-	return (
-		<Layout>
-			<Head />
-			<h1>Home page</h1>
+  // the log here will happen in the browser console
+  console.log(props);
+  return (
+    <Layout>
+      <Head />
+      <h1>Home page</h1>
 
-			{props.games.map((game) => {
-				return <h3 key={game.slug}>{game.name}</h3>;
-			})}
-		</Layout>
-	);
+      {props.games.map((game) => {
+        return <h3 key={game.slug}>{game.name}</h3>;
+      })}
+    </Layout>
+  );
 }
 
 export async function getStaticProps() {
-	// in case there is an error in the API call
-	// we'll send an empty array in as the prop
-	let games = [];
+  // in case there is an error in the API call
+  // we'll send an empty array in as the prop
+  let games = [];
 
-	try {
-		const response = await axios.get(BASE_URL);
-		// the log here will happen on the server, you can check the console in your editor
-		console.log(response.data);
-		// the array is on the response.data.data property
-		games = response.data.data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await axios.get(BASE_URL);
+    // the log here will happen on the server, you can check the console in your editor
+    // console.log(response.data);
+    // the array is on the response.data.results property
+    console.log(response.data.results);
+    games = response.data.results;
+  } catch (error) {
+    console.log(error);
+  }
 
-	// the props object we return here will become the props in the component
-	return {
-		props: {
-			games: games,
-		},
-	};
+  // the props object we return here will become the props in the component
+  return {
+    props: {
+      games: games,
+    },
+  };
 }
